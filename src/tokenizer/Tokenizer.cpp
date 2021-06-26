00f58b4cdfb41295c27b1ff4720cb0751fc40ddb
Vasilii Generalov <vasilii.generalov@gmail.com>
2021-06-26 20:32:59 +0300

    feat(tokenizer): update tokenizer funcs and classes

--- src/tokenizer/Tokenizer.cpp
+++ src/tokenizer/Tokenizer.cpp
@@ -1,1 +1,183 @@
+#include <vector>
+#include <iostream>
+#include <string>
+
 #include "Tokenizer.hpp"
+
+namespace tokenizer {
+
+  TokenInt::TokenInt(int newValue) : value(newValue)
+  {
+    std::cout << "created TokenInt with " << value << std::endl;
+  }
+
+  TokenFloat::TokenFloat(double newValue) : value(newValue)
+  {
+    std::cout << "created TokenFloat with " << value << std::endl;
+  }
+
+  TokenAdd::TokenAdd()
+  {
+    std::cout << "created TokenAdd" << std::endl;
+  }
+
+  TokenSub::TokenSub()
+  {
+    std::cout << "created TokenSub" << std::endl;
+  }
+
+  TokenMul::TokenMul()
+  {
+    std::cout << "created TokenMul" << std::endl;
+  }
+
+  TokenDiv::TokenDiv()
+  {
+    std::cout << "created TokenDiv" << std::endl;
+  }
+
+  TokenOpenParen::TokenOpenParen()
+  {
+    std::cout << "created TokenOpenParen" << std::endl;
+  }
+
+  TokenCloseParen::TokenCloseParen()
+  {
+    std::cout << "created TokenCloseParen" << std::endl;
+  }
+
+  Token* createIntOrFloat(const std::string& s)
+  {
+    if (s.empty()) {
+      return new TokenInt{};
+    }
+    std::size_t i{0};
+    int sign{1};
+    if ('-' == s.at(0)) {
+      sign = -1;
+      ++i;
+    }
+    int order{10};
+    int whole{0};
+    bool isFloat{false};
+    for (; i < s.size(); ++i) {
+      char c = s.at(i);
+      if ('.' == c || ',' == c) {
+        isFloat = true;
+        break;
+      }
+      whole = order*whole + (c - '0');
+    }
+    if (isFloat) {
+      double frac{0};
+      order = 10;
+      ++i;
+      for (; i < s.size(); ++i) {
+        char c = s.at(i);
+        frac = frac + (static_cast<double>(c - '0') / order);
+        order *= 10;
+      }
+      return new TokenFloat(sign * (static_cast<double>(whole) + frac));
+    }
+    else {
+      return new TokenInt(sign * whole);
+    }
+  }
+
+  std::vector<Token*> tokenize(const std::string& input)
+  {
+    std::vector<Token*> result{};
+    bool isToken{false};
+    std::string tmp;
+    for (std::size_t i = 0; i < input.size(); ++i) {
+      char c;
+      c = input.at(i);
+      if (' ' == c) {
+        if (isToken) {
+          isToken = false;
+          std::cout << tmp << std::endl;
+          result.push_back(createIntOrFloat(tmp));
+          tmp.erase();
+        }
+      }
+      if ((c >= '0' && c <= '9') || '.' == c || ',' == c) {
+        if (isToken) {
+          tmp.push_back(c);
+        }
+        else {
+          isToken = true;
+          tmp.push_back(c);
+        }
+      }
+      if ('+' == c) {
+        if (isToken) {
+          isToken = false;
+          std::cout << tmp << std::endl;
+          result.push_back(createIntOrFloat(tmp));
+          tmp.erase();
+        }
+        result.push_back(new TokenAdd{});
+      }
+      if ('-' == c) {
+        if (isToken) {
+          isToken = false;
+          std::cout << tmp << std::endl;
+          result.push_back(createIntOrFloat(tmp));
+          tmp.erase();
+          result.push_back(new TokenSub{});
+        }
+        else {
+          isToken = true;
+          tmp.push_back(c);
+        }
+      }
+      if ('*' == c) {
+        if (isToken) {
+          isToken = false;
+          std::cout << tmp << std::endl;
+          result.push_back(createIntOrFloat(tmp));
+          tmp.erase();
+        }
+        result.push_back(new TokenMul{});
+      }
+      if ('/' == c) {
+        if (isToken) {
+          isToken = false;
+          std::cout << tmp << std::endl;
+          result.push_back(createIntOrFloat(tmp));
+          tmp.erase();
+        }
+        result.push_back(new TokenDiv{});
+      }
+      if ('(' == c) {
+        if (isToken) {
+          isToken = false;
+          std::cout << tmp << std::endl;
+          result.push_back(createIntOrFloat(tmp));
+          tmp.erase();
+        }
+        result.push_back(new TokenOpenParen{});
+      }
+      if (')' == c) {
+        if (isToken) {
+          isToken = false;
+          std::cout << tmp << std::endl;
+          result.push_back(createIntOrFloat(tmp));
+          tmp.erase();
+        }
+        result.push_back(new TokenCloseParen{});
+      }
+    }
+    std::cout << tmp << std::endl;
+    result.push_back(createIntOrFloat(tmp));
+    return result;
+  }
+
+  void cleanup(const std::vector<Token*> v)
+  {
+    for (const auto p : v) {
+      delete p;
+    }
+  }
+
+} // namespace tokenizer
--- src/tokenizer/Tokenizer.hpp
+++ src/tokenizer/Tokenizer.hpp
@@ -1,4 +1,77 @@
 #ifndef TOKENIZER_HPP
 #define TOKENIZER_HPP
 
-#endif
+#include <vector>
+#include <string>
+
+namespace tokenizer {
+
+  class Token {
+    public:
+      Token() = default;
+      virtual ~Token() = default;
+  };
+
+  class TokenInt : public Token {
+    public:
+      TokenInt() = default;
+      explicit TokenInt(int newValue);
+      ~TokenInt() override = default;
+    private:
+      int value;
+  };
+
+  class TokenFloat : public Token {
+    public:
+      TokenFloat() = default;
+      explicit TokenFloat(double newValue);
+      ~TokenFloat() override = default;
+    private:
+      double value;
+  };
+
+  class TokenAdd : public Token {
+    public:
+      TokenAdd();
+      ~TokenAdd() override = default;
+  };
+
+  class TokenSub : public Token {
+    public:
+      TokenSub();
+      ~TokenSub() override = default;
+  };
+
+  class TokenMul : public Token {
+    public:
+      TokenMul();
+      ~TokenMul() override = default;
+  };
+
+  class TokenDiv : public Token {
+    public:
+      TokenDiv();
+      ~TokenDiv() override = default;
+  };
+
+  class TokenOpenParen : public Token {
+    public:
+      TokenOpenParen();
+      ~TokenOpenParen() override = default;
+  };
+
+  class TokenCloseParen : public Token {
+    public:
+      TokenCloseParen();
+      ~TokenCloseParen() override = default;
+  };
+
+  Token* createIntOrFloat(const std::string& s);
+
+  std::vector<Token*> tokenize(const std::string& input);
+
+  void cleanup(const std::vector<Token*> v);
+
+} // namespace tokenizer
+
+#endif // TOKENIZER_HPP
