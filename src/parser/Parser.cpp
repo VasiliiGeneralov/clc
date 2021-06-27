0bf0cd4ef643553d4098ff8a96777d3c89609dce
Vasilii Generalov <vasilii.generalov@gmail.com>
2021-06-27 14:22:15 +0300

    feat(parser): write parser as a singleton

--- src/parser/Parser.cpp
+++ src/parser/Parser.cpp
@@ -1,1 +1,14 @@
+#include <vector>
+#include <string>
+
 #include "Parser.hpp"
+
+Parser& Parser::get()
+{
+  static Parser p;
+  return p;
+}
+
+std::vector<std::string> Parser::parse(const std::vector<std::string>& v)
+{
+}
--- src/parser/Parser.hpp
+++ src/parser/Parser.hpp
@@ -1,4 +1,18 @@
 #ifndef PARSER_HPP
 #define PARSER_HPP
 
-#endif
+#include <vector>
+#include <string>
+
+class Parser {
+  public:
+    explicit Parser(const Parser& p) = delete;
+    Parser& operator=(const Parser& p) = delete;
+    ~Parser() = default;
+    static Parser& get();
+    static std::vector<std::string> parse(const std::vector<std::string>& v);
+  private:
+    Parser() = default;
+};
+
+#endif // PARSER_HPP
