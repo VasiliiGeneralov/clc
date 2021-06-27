a0cc65bd329fe86b9378f5916475c9e214b2a75c
Vasilii Generalov <vasilii.generalov@gmail.com>
2021-06-28 00:05:49 +0300

    feat(analyzer): create parsed tokens analyzer

--- src/analyzer/Analyzer.cpp
+++ src/analyzer/Analyzer.cpp
@@ -1,1 +1,81 @@
+#include <string>
+#include <tuple>
+#include <vector>
+
 #include "Analyzer.hpp"
+
+Analyzer& Analyzer::get()
+{
+  static Analyzer a;
+  return a;
+}
+
+std::tuple<bool, int, double> Analyzer::analyze(const std::vector<std::string>& v)
+{
+  return Analyzer::get().traverse(v.begin(), v.end() - 1);
+}
+
+std::tuple<bool, int, double> Analyzer::traverse(std::vector<std::string>::const_iterator first,
+                                                 std::vector<std::string>::const_iterator last)
+{
+  if ("+" == *last) {
+    if (floatMode) {
+      return std::tuple(floatMode, 0, std::get<2>(traverse(first, last - 2)) + std::get<2>(traverse(first, last - 1)));
+    }
+    return std::tuple(floatMode, std::get<1>(traverse(first, last - 2)) + std::get<1>(traverse(first, last - 1)), 0);
+  }
+  if ("-" == *last) {
+    if (floatMode) {
+      return std::tuple(floatMode, 0, std::get<2>(traverse(first, last - 2)) - std::get<2>(traverse(first, last - 1)));
+    }
+    return std::tuple(floatMode, std::get<1>(traverse(first, last - 2)) - std::get<1>(traverse(first, last - 1)), 0);
+  }
+  if ("*" == *last) {
+    if (floatMode) {
+      return std::tuple(floatMode, 0, std::get<2>(traverse(first, last - 2)) * std::get<2>(traverse(first, last - 1)));
+    }
+    return std::tuple(floatMode, std::get<1>(traverse(first, last - 2)) * std::get<1>(traverse(first, last - 1)), 0);
+  }
+  if ("/" == *last) {
+    if (!floatMode) {
+      floatMode = true;
+    }
+    return std::tuple(floatMode, 0, std::get<2>(traverse(first, last - 2)) / std::get<2>(traverse(first, last - 1)));
+  }
+  return create(*last);
+}
+
+std::tuple<bool, int, double> Analyzer::create(const std::string& s)
+{
+  std::size_t i{0};
+  int sign{1};
+  if ('-' == s.at(0)) {
+    sign = -1;
+    ++i;
+  }
+  int order{10};
+  int whole{0};
+  bool isFloat{false};
+  for (; i < s.size(); ++i) {
+    char c = s.at(i);
+    if ('.' == c || ',' == c) {
+      isFloat = true;
+      break;
+    }
+    whole = order*whole + (c - '0');
+  }
+  double frac{0};
+  if (isFloat) {
+    order = 10;
+    ++i;
+    for (; i < s.size(); ++i) {
+      char c = s.at(i);
+      frac += (static_cast<double>(c - '0') / order);
+      order *= 10;
+    }
+    if (!floatMode) {
+      floatMode = true;
+    }
+  }
+  return std::tuple(floatMode, sign * whole, static_cast<double>(sign * whole) + frac);
+}
--- src/analyzer/Analyzer.hpp
+++ src/analyzer/Analyzer.hpp
@@ -1,4 +1,23 @@
 #ifndef ANALYZER_HPP
 #define ANALYZER_HPP
 
-#endif
+#include <string>
+#include <tuple>
+#include <vector>
+
+class Analyzer {
+  public:
+    explicit Analyzer(const Analyzer& a) = delete;
+    Analyzer& operator=(const Analyzer& a) = delete;
+    ~Analyzer() = default;
+    static Analyzer& get();
+    static std::tuple<bool, int, double> analyze(const std::vector<std::string>& v);
+  private:
+    Analyzer() = default;
+    std::tuple<bool, int, double> traverse(std::vector<std::string>::const_iterator first,
+                                           std::vector<std::string>::const_iterator last);
+    std::tuple<bool, int, double> create(const std::string& s);
+    bool floatMode;
+};
+
+#endif // ANALYZER_HPP
